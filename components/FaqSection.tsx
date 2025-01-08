05f90fec1140d7eab5878a9935810c24a7e538cf
davrec <metplus01@gmail.com>
2025-01-09 00:26:21 +0100

    Update FaqSection.tsx

--- components/FaqSection.tsx
+++ components/FaqSection.tsx
@@ -15,7 +15,7 @@ export function FAQSection() {
         <div className="flex flex-col sm:flex-row justify-between gap-8 w-full">
           {/* Card 1 */}
           <div className="flex-1 bg-bgScuro p-6 rounded-lg shadow-lg">
-            <h4 className="text-2xl font-semibold mb-4 text-primary">Che cos&apos;è Urban enigma?</h4>
+            <h4 className="text-2xl font-semibold mb-4 text-white">Che cos&apos;è Urban enigma?</h4>
             <p className="text-lg leading-relaxed">
             Urban enigma è un&apos;esperienza di gioco all&apos;aperto che trasforma la città di Napoli in un grande campo di gioco. 
             Attraverso percorsi interattivi, i giocatori risolvono enigmi e scoprono installazioni nascoste guidati dalla nostra app dedicata.
@@ -24,7 +24,7 @@ export function FAQSection() {
 
           {/* Card 2 */}
           <div className="flex-1 bg-bgScuro p-6 rounded-lg shadow-lg">
-            <h4 className="text-2xl font-semibold mb-4 text-primary">Come funziona il gioco?</h4>
+            <h4 className="text-2xl font-semibold mb-4 text-white">Come funziona il gioco?</h4>
             <p className="text-lg leading-relaxed">
             Dopo aver scelto e acquistato un percorso tramite la nostra app, riceverai istruzioni su dove iniziare. 
             Ogni giocatore avrà strumenti diversi sul proprio smartphone, come mappe, bussole o funzioni di messaggistica, per collaborare e risolvere gli enigmi lungo il percorso.
@@ -33,7 +33,7 @@ export function FAQSection() {
 
           {/* Card 3 */}
           <div className="flex-1 bg-bgScuro p-6 rounded-lg shadow-lg">
-            <h4 className="text-2xl font-semibold mb-4 text-primary">Quante persone possono partecipare?</h4>
+            <h4 className="text-2xl font-semibold mb-4 text-white">Quante persone possono partecipare?</h4>
             <p className="text-lg leading-relaxed">
             I nostri percorsi sono progettati per gruppi da 2 a 4 persone. Tuttavia, è possibile adattare l&apos;esperienza per gruppi più grandi, suddividendoli in squadre che possono competere tra loro.
             </p>
@@ -42,7 +42,7 @@ export function FAQSection() {
 
         {/* Testo aggiuntivo in basso a destra */}
         <div className="w-full mt-10 text-right">
-          <p className="text-lg text-bgScuro">
+          <p className="text-lg text-white">
             Per leggerne di più{' '}
             <Link href="/faq" className="text-white font-semibold hover:underline">
               clicca qui
